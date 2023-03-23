b8feca2bb5a2c133bbdc9460ae83e5ec6bde6820
scivision <scivision@users.noreply.github.com>
2023-03-23 09:45:53 -0400

    cmake: update ExtProj.cmake and GetJson.cmake from Gemini3d/External

--- cmake/ExtProj.cmake
+++ cmake/ExtProj.cmake
@@ -1,8 +1,7 @@
 include(ExternalProject)
 
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  include(${CMAKE_CURRENT_LIST_DIR}/Modules/JsonParse.cmake)
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/GetJson.cmake)
+
 
 function(extproj name url_type cmake_args depends)
 
@@ -13,19 +12,52 @@ list(INSERT cmake_args 0
 -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
 -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 -DBUILD_TESTING:BOOL=false
+-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+-DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
 )
 if(CMAKE_TOOLCHAIN_FILE)
   list(APPEND cmake_args -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE})
 endif()
 
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  sbeParseJson(meta json_meta)
-  set(url ${meta.${name}.url})
-else()
-  string(JSON url GET ${json_meta} ${name} url)
+set(extproj_args
+CMAKE_ARGS ${cmake_args}
+TLS_VERIFY true
+DEPENDS ${depends}
+)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
+  list(APPEND extproj_args GIT_REMOTE_UPDATE_STRATEGY "CHECKOUT")
 endif()
 
-if(local)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)
+  list(APPEND extproj_args
+  INACTIVITY_TIMEOUT 60
+  CONFIGURE_HANDLED_BY_BUILD true
+  )
+endif()
+
+# --- cache_args for repos that need list args
+
+
+# --- select repo type
+
+get_url(${name} "${json_meta}")
+
+if(url_type STREQUAL "source_dir")
+  # local development with a specific source directory out of tree
+
+  get_filename_component(${name}_source ${${name}_source} ABSOLUTE)
+
+  message(STATUS "${name}: local source development directory: ${${name}_source}")
+  ExternalProject_Add(${name}
+  SOURCE_DIR ${${name}_source}
+  BUILD_ALWAYS false
+  ${extproj_args}
+  )
+  # NOTE: "BUILD_ALWAYS true" is suggested by ExternalProject_Add()
+  # docs when SOURCE_DIR is used w/o Download step, if source_dir changes aren't being detected.
+
+elseif(url_type STREQUAL "local")
   # archive file on this computer or network drive
 
   find_file(${name}_archive
@@ -35,56 +67,71 @@ if(local)
   )
 
   if(NOT ${name}_archive)
-    message(FATAL_ERROR "Archive file for ${name} does not exist under ${local}")
+    message(FATAL_ERROR "${name}: Archive file does not exist under ${local}")
   endif()
 
-  message(STATUS "${name}: using source archive ${${name}_archive}")
+  if(name STREQUAL "hdf5")
+    # special handling due to custom HDF5 archive layout
+    # need to strip extra directories HDF_Group/HDF5/${HDF5_VERSION}
+    find_program(tar NAMES tar)
+    if(NOT tar)
+      message(FATAL_ERROR "Could not find tar program")
+    endif()
 
-  ExternalProject_Add(${name}
-  URL ${${name}_archive}
-  CMAKE_ARGS ${cmake_args}
-  CONFIGURE_HANDLED_BY_BUILD true
-  TEST_COMMAND ""
-  DEPENDS ${depends}
-  )
+    set(_ext_src ${PROJECT_BINARY_DIR}/${name}_archive)
+    file(MAKE_DIRECTORY ${_ext_src})
+
+    execute_process(
+    COMMAND ${tar} --extract --strip-components=4 --directory ${_ext_src} --file ${${name}_archive}
+    RESULT_VARIABLE ret
+    )
+    if(NOT ret EQUAL "0")
+      message(FATAL_ERROR "${name}: could not extract source archive ${${name}_archive}")
+    endif()
+
+    message(STATUS "${name}: using extracted source ${_ext_src}")
+
+    ExternalProject_Add(${name}
+    SOURCE_DIR ${_ext_src}
+    TEST_COMMAND ""
+    ${extproj_args}
+    )
+
+  else()
+    # default archive without extra custom top-level directories
+    message(STATUS "${name}: using source archive ${${name}_archive}")
+
+    ExternalProject_Add(${name}
+    URL ${${name}_archive}
+    TEST_COMMAND ""
+    ${extproj_args}
+    )
+  endif()
 
 elseif(url_type STREQUAL "git")
 
-  if(CMAKE_VERSION VERSION_LESS 3.19)
-    set(tag ${meta.${name}.tag})
-  else()
-    string(JSON tag GET ${json_meta} ${name} tag)
-  endif()
+  get_tag(${name} "${json_meta}")
 
   ExternalProject_Add(${name}
   GIT_REPOSITORY ${url}
   GIT_TAG ${tag}
   GIT_SHALLOW true
-  CMAKE_ARGS ${cmake_args}
-  INACTIVITY_TIMEOUT 60
-  CONFIGURE_HANDLED_BY_BUILD true
+  GIT_PROGRESS true
   TEST_COMMAND ""
-  DEPENDS ${depends}
+  ${extproj_args}
   )
 elseif(url_type STREQUAL "archive")
 
-  if(CMAKE_VERSION VERSION_LESS 3.19)
-    set(sha256 ${meta.${name}.sha256})
-  else()
-    string(JSON sha256 GET ${json_meta} ${name} sha256)
-  endif()
+  get_hash(${name} "${json_meta}")
 
   ExternalProject_Add(${name}
   URL ${url}
   URL_HASH SHA256=${sha256}
-  CMAKE_ARGS ${cmake_args}
-  INACTIVITY_TIMEOUT 60
-  CONFIGURE_HANDLED_BY_BUILD true
   TEST_COMMAND ""
-  DEPENDS ${depends}
+  ${extproj_args}
   )
 else()
-  message(FATAL_ERROR "unsure how to use resource of type ${url_type}")
+  message(FATAL_ERROR "${name}: unsure how to use resource of type ${url_type}")
 endif()
 
 endfunction(extproj)
--- options.cmake
+++ options.cmake
@@ -1,4 +1,4 @@
-message(STATUS "${PROJECT_NAME} CMake ${CMAKE_VERSION} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
+message(STATUS "${PROJECT_NAME} CMake ${CMAKE_VERSION}  ${CMAKE_SYSTEM_NAME} Toolchain ${CMAKE_TOOLCHAIN_FILE}")
 
 # --- user options
 
@@ -12,29 +12,19 @@ if(local)
   endif()
 endif()
 
+# --- other options
+
 if(NOT DEFINED CRAY AND DEFINED ENV{CRAYPE_VERSION})
   set(CRAY true)
 endif()
 
 set(CMAKE_TLS_VERIFY true)
 
-# --- config checks
-
-get_property(is_multi_config GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-if(is_multi_config OR CMAKE_GENERATOR MATCHES "Visual Studio")
-  if(CMAKE_GENERATOR MATCHES "Ninja")
-    set(suggest Ninja)
-  elseif(WIN32)
-    set(suggest "MinGW Makefiles")
-  else()
-    set(suggest "Unix Makefiles")
-  endif()
-  message(FATAL_ERROR "Please use a single configuration generator like:
-  cmake -G \"${suggest}\"
-  ")
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  message(FATAL_ERROR "please define library install prefix like:
+  cmake -DCMAKE_INSTALL_PREFIX=/path/to/install")
 endif()
-
-# --- exclude Conda from search
+# --- exclude Conda from search - this avoids incompatible libraries being found
 if(DEFINED ENV{CONDA_PREFIX})
   set(ignore_path
     $ENV{CONDA_PREFIX} $ENV{CONDA_PREFIX}/Library $ENV{CONDA_PREFIX}/Scripts $ENV{CONDA_PREFIX}/condabin
@@ -49,23 +39,17 @@ if(NOT DEFINED CMAKE_PREFIX_PATH AND DEFINED ENV{CMAKE_MODULE_PATH})
   set(CMAKE_PREFIX_PATH $ENV{CMAKE_MODULE_PATH})
 endif()
 if(CMAKE_PREFIX_PATH)
-  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.21)
-    file(REAL_PATH ${CMAKE_PREFIX_PATH} CMAKE_PREFIX_PATH EXPAND_TILDE)
-  else()
-    get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
-  endif()
+  get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
+
   list(APPEND CMAKE_MODULE_PATH ${CMAKE_PREFIX_PATH}/cmake)
 endif()
 
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
 
 # --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
-  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
+if(NOT PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
+  file(GENERATE OUTPUT .gitignore CONTENT "*")
 endif()
 
 # --- check for updated external projects when "false"
 set_property(DIRECTORY PROPERTY EP_UPDATE_DISCONNECTED false)
-
-# --- read JSON with URLs for each library
-file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json_meta)
